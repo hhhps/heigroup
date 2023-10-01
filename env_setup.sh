592d72310af975a0dc05b9236bea9efab7100b57
hhhps <jameshhhps@gmail.com>
2023-10-01 21:12:19 +0800

    created env setup and testing files

---
+++ env_setup.sh
@@ -0,0 +1,4 @@
+conda create --name heipy anaconda conda=23.5.2 python=3.11 scikit-learn=1.3 mysqlclient=2.0
+conda activate heipy
+conda install python-dotenv -c conda-forge
+conda install jupysql -c conda-forge
